1582137774ac6d36cd6e8563fab5204628ddd9f1
Hassan Wael <hassan.wael@brightskiesinc.com>
2021-12-15 17:51:53 +0200

    add commit to test magic numbers

--- src/main/java/TestProject/demo/DemoApplication.java
+++ src/main/java/TestProject/demo/DemoApplication.java
@@ -1,15 +1,17 @@
 package TestProject.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		for (int i=0; i < 10; i++) {
-			System.out.println("hassan");
+			logger.error("hassan");
 		}
 	}
 
